72f4463b4558d9b5c0a997bb22245bfac6a38548
ghostletters <ghost.letters@mailbox.org>
2022-03-17 20:44:14 +0100

    fix compile; upgrade to java 17

--- src/main/java/xyz/ghostletters/common/repository/AuthorRepository.java
+++ src/main/java/xyz/ghostletters/common/repository/AuthorRepository.java
@@ -3,5 +3,5 @@ package xyz.ghostletters.common.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import xyz.ghostletters.common.entity.Author;
 
-public interface AutherRepository extends JpaRepository<Author, Long> {
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
